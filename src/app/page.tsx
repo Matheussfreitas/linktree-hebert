1c7f6f762e3533fbe6980ea9d17e37d1b49805d0
Matheus Freitas <mgsoutodelira@gmail.com>
2025-08-14 11:30:26 -0300

    up cores and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,6 @@ import {
 export default function Home() {
   const links = [
     {
-      icon: <FaLinkedin />,
-      label: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/vinihebert/',
-    },
-    {
       icon: <FaBehanceSquare />,
       label: 'Behance',
       url: 'https://www.behance.net/vinihebert',
@@ -26,11 +21,17 @@ export default function Home() {
       url: 'https://wa.me/5584988994447?text=Fala%20Vini!%20Dei%20uma%20olhada%20nas%20suas%20produ%C3%A7%C3%B5es%20e%20tenho%20interesse%20no%20seu%20trampo%20🚀',
     },
     {
+      icon: <FaLinkedin />,
+      label: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/vinihebert/',
+    },
+    {
       icon: <FaYoutube />,
       label: 'YouTube',
       url: 'https://youtube.com',
     },
   ];
+  
   return (
     <>
       <div className="fixed inset-0 w-full min-h-screen z-0">
@@ -51,7 +52,7 @@ export default function Home() {
             <h1 className="text-2xl font-bold mt-4 drop-shadow-md">
               VINI HEBERT
             </h1>
-            <p className="bg-gradient-to-r from-yellow-400 to-red-600 bg-clip-text text-transparent drop-shadow-lg">
+            <p className="bg-gradient-to-r from-white from-20% to-zinc-600 bg-clip-text text-transparent drop-shadow-lg">
               Designer & Videomaker
             </p>
           </div>
@@ -62,7 +63,7 @@ export default function Home() {
             <Link
               key={link.url}
               href={link.url}
-              className="block p-[1px] rounded-lg bg-gradient-to-r from-yellow-500 from-75% to-red-600"
+              className="block p-[1px] rounded-lg bg-gradient-to-l from-white from-65% to-zinc-600"
             >
               <div className="bg-black/95 hover:bg-zinc-900 backdrop-blur-sm text-white px-6 py-3 rounded-lg transition-all duration-200 shadow-lg transform text-center flex items-center justify-center gap-2">
                 <span>{link.icon}</span>
